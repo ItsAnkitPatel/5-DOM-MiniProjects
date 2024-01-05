41014ed9dec89a6d5a3d23ad1a79835615d852f7
ItsAnkitPatel <itsakppatel@gmail.com>
2024-01-05 22:18:11 +0530

    Updated script.js files

--- 05_Insta_Story_Feature/script.js
+++ 05_Insta_Story_Feature/script.js
@@ -10,6 +10,11 @@ const arr = [
       "https://i.pinimg.com/564x/3c/55/45/3c55457fee2a871acc3d117052430943.jpg",
   },
   {
+    dp: "https://i.pinimg.com/564x/c8/b7/41/c8b7413ccd33544ab59691a09a7b4107.jpg",
+    story:
+      "https://i.pinimg.com/564x/9c/30/44/9c30449ead2be85773673ae4e6fdd58c.jpg",
+  },
+  {
     dp: "https://i.pinimg.com/564x/d1/6e/02/d16e02b9df2b696adaae072b13eea27e.jpg",
     story:
       "https://i.pinimg.com/736x/07/0c/f2/070cf282fc54b07ff51d44c026d86b13.jpg",
@@ -23,12 +28,6 @@ const arr = [
     dp: "https://i.pinimg.com/564x/cc/95/e1/cc95e133a7043e4adcb4c81e4e68785b.jpg",
     story:
       "https://i.pinimg.com/564x/b7/48/97/b74897a8f64bff71e70433f88ee982e4.jpg",
-  },
-
-  {
-    dp: "https://i.pinimg.com/564x/c8/b7/41/c8b7413ccd33544ab59691a09a7b4107.jpg",
-    story:
-      "https://i.pinimg.com/564x/9c/30/44/9c30449ead2be85773673ae4e6fdd58c.jpg",
   },
 ];
 
@@ -64,7 +63,7 @@ const fullScreenImage = document.querySelector("#full-screen-image");
 
 storyHeader.addEventListener("click", function (e) {
 
-  if (!isNaN(e.target.id)) {//If the user clicked on the story header then ignore it
+  if ((e.target.id)) {//If the user clicked on the story header then ignore it
     
     let imgValue = arr[e.target.id].story;
 
@@ -78,3 +77,4 @@ storyHeader.addEventListener("click", function (e) {
     },2000)
   }
 });
+
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const main = document.querySelector("main");
 const cursor = document.querySelector("#cursor");
-const anchors = document.querySelectorAll("li")
+const anchors = document.querySelectorAll("li a")
 main.addEventListener("mousemove", function (e) {
   cursor.style.left = e.x + "px";
   cursor.style.top = e.y + "px";
