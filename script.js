ad044c7e8234dcb890b1341cd23d0a61fd996179
ItsAnkitPatel <itsakppatel@gmail.com>
2024-01-05 19:15:01 +0530

    Front Page for the Project

---
+++ script.js
@@ -0,0 +1,18 @@
+const main = document.querySelector("main");
+const cursor = document.querySelector("#cursor");
+const anchors = document.querySelectorAll("li")
+main.addEventListener("mousemove", function (e) {
+  cursor.style.left = e.x + "px";
+  cursor.style.top = e.y + "px";
+});
+
+anchors.forEach(function(e){
+    e.addEventListener("mousemove",function(){
+        cursor.style.width ="60px"
+        cursor.style.height ="60px"
+    })
+    e.addEventListener("mouseleave",function(){
+        cursor.style.width ="20px"
+        cursor.style.height ="20px"
+    })
+})
